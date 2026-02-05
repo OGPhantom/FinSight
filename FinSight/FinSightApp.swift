8c0191bab6c8c76c00d0bde9a25a236bd0c30108
OGPhantom <nikita.storchai@nure.ua>
2026-02-05 23:23:30 +0200

    Initial commit

---
+++ FinSight/FinSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinSightApp.swift
+//  FinSight
+//
+//  Created by Никита Сторчай on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct FinSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
