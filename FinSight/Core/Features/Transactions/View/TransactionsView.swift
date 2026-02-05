62c8b2566811eb07fef5b7bad2ae265c95141281
OGPhantom <nikita.storchai@nure.ua>
2026-02-06 00:00:49 +0200

    Create TransactionsView with empty state UI

---
+++ FinSight/Core/Features/Transactions/View/TransactionsView.swift
@@ -0,0 +1,50 @@
+//
+//  TransactionsView.swift
+//  FinSight
+//
+//  Created by Никита Сторчай on 05.02.2026.
+//
+
+import SwiftUI
+
+struct TransactionsView: View {
+    @State private var transactions: [Transaction] = []
+    @State private var showingAdd = false
+    var body: some View {
+        NavigationStack {
+            Group {
+                if transactions.isEmpty {
+                    ContentUnavailableView("No Transactions", systemImage: "tray", description: Text("Add your first transaction to get started"))
+                        .padding()
+                } else {
+                    List {
+                        ForEach(transactions) { transaction in
+                            // TransactionRow(transaction: transaction)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Transactions")
+            .sheet(isPresented: $showingAdd, content: {
+                Text("Add Transaction here")
+            })
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showingAdd = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    TransactionsView()
+}
