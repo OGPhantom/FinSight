3257c26a93908daffa8b75c74bc38abe483acc9e
OGPhantom <nikita.storchai@nure.ua>
2026-02-07 15:16:37 +0200

    iOS 26

---
+++ FinSight/Core/Features/Reports/Model/FinanceSummaryInput.swift
@@ -0,0 +1,8 @@
+//
+//  FinanceSummaryInput.swift
+//  FinSight
+//
+//  Created by Никита Сторчай on 07.02.2026.
+//
+
+import Foundation
---
+++ FinSight/Core/Features/Reports/Model/FinanceSummaryOutput.swift
@@ -0,0 +1,8 @@
+//
+//  FinanceSummaryOutput.swift
+//  FinSight
+//
+//  Created by Никита Сторчай on 07.02.2026.
+//
+
+import Foundation
